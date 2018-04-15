7400c1416e61edfad0d2ad1959d54cc4a28d1d52
sunil gopinath <sunil.x.gopinath@gmail.com>
2018-04-16 00:45:32 +1000

    islucky

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	// h := basic.ProductExceptSelf([]int{1, 2, 3, 4}, 12)
 	// fmt.Println(h)
-	h := arcade.MatrixElementsSum([][]int{{1, 1, 1, 0}, {0, 5, 0, 1}, {2, 1, 3, 10}})
+	h := arcade.IsLucky(1230)
 	fmt.Println(h)
 }
